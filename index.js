3be89e10e448a01dbdc164ad3775fc1a04b995f2
Ian <ian.njoroge2@student.moringaschool.com>
2025-03-12 19:05:39 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+const broncosRecord = [
+    { year: "2012", result: "W" },
+    { year: "2014", result: "W" },
+    { year: "2015", result: "W" },
+    { year: "2010", result: "L" },
+    { year: "2013", result: "L" }
+];
+
+console.log(superbowlWin(broncosRecord));
